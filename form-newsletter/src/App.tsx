3595b5148a87d0dcc43900a71bd66fa3306496a9
Thiago Souza <thiagosouza.contato01@gmail.com>
2024-03-20 18:00:18 -0300

    adicionando o projeto formNewsletter

---
+++ form-newsletter/src/App.tsx
@@ -0,0 +1,12 @@
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-blue-600">Inscreva-se</h1>
+      <Form />
+    </>
+  );
+}
+
+export default App;
