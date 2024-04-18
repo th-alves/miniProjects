05d99df73bd8ad6a49a52e76512c0827c4b4bc27
Thiago Souza <thiagosouza.contato01@gmail.com>
2024-04-18 20:55:18 -0300

    Adicionando o projeto coffees

---
+++ coffee/assets/js/scripts.js
@@ -0,0 +1,15 @@
+document.getElementById("defaultOpen").click();
+
+function showCoffees(evt, coffeeName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(coffeeName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
